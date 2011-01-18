7e862d709ee99dca7d497e099b6d41fcafc648e8
Hugh Sanderson <gamehaxe@gmail.com>
2011-01-18 16:07:05 +0000

    Add some msvc10 options and some hxcpp depends

--- build-tool/BuildTool.hx
+++ build-tool/BuildTool.hx
@@ -588,8 +588,9 @@ class BuildTool
                 case "path" : 
                    var path = substitute(el.att.name);
                    var os = neko.Sys.systemName();
-                   var sep = (new EReg("window","i")).match(os) ? ";" : ":";
+                   var sep = mDefines.exists("windows_host") ? ";" : ":";
                    neko.Sys.putEnv("PATH", path + sep + neko.Sys.getEnv("PATH"));
+                    //trace(neko.Sys.getEnv("PATH"));
                 case "compiler" : 
                    mCompiler = createCompiler(el,mCompiler);
 
@@ -943,8 +944,11 @@ class BuildTool
             neko.Sys.setCwd(last);
          }
       }
-   
+
       var os = neko.Sys.systemName();
+      var windows_host = (new EReg("window","i")).match(os);
+		if (windows_host)
+		   defines.set("windows_host", "1");
 
       for(arg in args)
       {
@@ -1018,6 +1022,40 @@ class BuildTool
          defines.set("windows","windows");
          defines.set("BINDIR",m64 ? "Windows64":"Windows");
 
+			if (defines.exists("msvc10"))
+			{
+				var vc_setup_proc = new neko.io.Process("cmd.exe", ["/C", HXCPP + "build-tool\\msvc10-setup.bat" ]);
+				var vars_found = false;
+				try{
+				   while(true)
+				   {
+               	var str = vc_setup_proc.stdout.readLine();
+						if (str=="HXCPP_VARS")
+							vars_found = true;
+						else if (!vars_found)
+				      	neko.Lib.println(str);
+						else
+						{
+						   var pos = str.indexOf("=");
+							var name = str.substr(0,pos);
+							switch(name)
+							{
+							   case "PATH", "VCINSTALLDIR", "WindowsSdkDir","Framework35Version",
+								   "FrameworkDir", "FrameworkDIR32", "FrameworkVersion",
+									"FrameworkVersion32", "DevEnvDir", "INCLUDE", "LIB", "LIBPATH"
+								 :
+							      var value = str.substr(pos+1);
+							      //trace(name + " = " + value);
+					            defines.set(name,value);
+                           neko.Sys.putEnv(name,value);
+							}
+						}
+				   } 
+				} catch (e:Dynamic) { };
+            vc_setup_proc.close();
+			}
+
+			var cl_version = "";
          try
          {
             var proc = new neko.io.Process("cl.exe",[]);
@@ -1027,10 +1065,16 @@ class BuildTool
             {
                 var reg = ~/Version\s+(\d+)/i;
                 if (reg.match(str))
+					 {
+					 	cl_version = reg.matched(1);
                    BuildTool.sAllowNumProcs = Std.parseInt(reg.matched(1)) >= 14;
+					 }
             }
          }
          catch(e:Dynamic){}
+
+			if (cl_version!="")
+			   neko.Lib.println("Using cl version: " + cl_version);
       }
       else if ( (new EReg("linux","i")).match(os) )
       {
@@ -1058,7 +1102,7 @@ class BuildTool
          for(e in env.keys())
             defines.set(e, neko.Sys.getEnv(e) );
 
-         if (defines.exists("HXCPP_CONFIG") )
+         if ( !defines.exists("HXCPP_CONFIG") )
             defines.set("HXCPP_CONFIG",".hxcpp_config.xml");
 
          if (defines.exists("mingw") && !defines.exists("MINGW_ROOT") )
---
+++ build-tool/msvc10-setup.bat
@@ -0,0 +1,3 @@
+@call "%VS100COMNTOOLS%\vsvars32.bat"
+@echo HXCPP_VARS
+@set
