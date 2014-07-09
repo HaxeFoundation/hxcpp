daee91c82bdbbccabffc21604d7bb8cfa24e74f9
hughsando <gamehaxe@gmail.com>
2014-07-09 19:07:12 +0800

    Use separate virtual function call to clear out stack variables and make gc test more reliable.

--- test/haxe/gc/TestGC.hx
+++ test/haxe/gc/TestGC.hx
@@ -7,68 +7,83 @@ class CustomObject {
 }
 
 class TestGC extends haxe.unit.TestCase {
+
+    function createAbc():Void {
+        var object = { test: "abc" };
+        Gc.doNotKill(object);
+        object = null;
+    }
     public function testObject():Void {
-        function innerFunction():Void {
-            var object = { test: "abc" };
-            Gc.doNotKill(object);
-            Gc.run(true);
-            assertTrue(Gc.getNextZombie() == null);
-        }();
+        createAbc();
         Gc.run(true);
         var zombie = Gc.getNextZombie();
         assertTrue(zombie != null);
         assertEquals("abc", zombie.test);
+        Gc.run(true);
+        assertTrue(Gc.getNextZombie() == null);
     }
 
-    public function testBoxedInt():Void {
-        function innerFunction():Void {
+    function create123():Void {
             var object:Null<Int> = 123;
             Gc.doNotKill(object);
-            Gc.run(true);
-            assertTrue(Gc.getNextZombie() == null);
-        }();
+    };
+ 
+    public function testBoxedInt():Void {
+        create123();
         Gc.run(true);
         var zombie:Dynamic = Gc.getNextZombie();
         assertTrue(zombie != null);
         assertEquals(123, zombie);
+        Gc.run(true);
+        assertTrue(Gc.getNextZombie() == null);
     }
 
+    function createFunction():Void {
+        var object = function() return "abc";
+        Gc.doNotKill(object);
+        object = null;
+    };
+
     public function testFunc():Void {
-        function innerFunction():Void {
-            var object = function() return "abc";
-            Gc.doNotKill(object);
-            Gc.run(true);
-            assertTrue(Gc.getNextZombie() == null);
-        }();
+        createFunction();
         Gc.run(true);
         var zombie = Gc.getNextZombie();
         assertTrue(zombie != null);
         assertEquals("abc", zombie());
+        Gc.run(true);
+        assertTrue(Gc.getNextZombie() == null);
     }
 
+    function createCustom():Void {
+        var object = new CustomObject();
+        Gc.doNotKill(object);
+        object = null;
+    };
+ 
     public function testCustomObject():Void {
-        function innerFunction():Void {
-            var object = new CustomObject();
-            Gc.doNotKill(object);
-            Gc.run(true);
-            assertTrue(Gc.getNextZombie() == null);
-        }();
+        createCustom();
         Gc.run(true);
         var zombie = Gc.getNextZombie();
         assertTrue(zombie != null);
         assertTrue(Std.is(zombie, CustomObject));
+        Gc.run(true);
+        assertTrue(Gc.getNextZombie() == null);
     }
 
+    function createBytes():Void {
+        var object = Bytes.alloc(1);
+        Gc.doNotKill(object);
+        object = null;
+    };
+  
+
     public function testBytes():Void {
-        function innerFunction():Void {
-            var object = Bytes.alloc(1);
-            Gc.doNotKill(object);
-            Gc.run(true);
-            assertTrue(Gc.getNextZombie() == null);
-        }();
+        createBytes();
         Gc.run(true);
         var zombie = Gc.getNextZombie();
         assertTrue(zombie != null);
         assertTrue(Std.is(zombie, Bytes));
+        Gc.run(true);
+        assertTrue(Gc.getNextZombie() == null);
     }
 }
