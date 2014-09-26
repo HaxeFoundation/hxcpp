ad7a714b200f8e5a73f28163d453c0304161a9b9
Alex Mokrushyn <codeservice@gmail.com>
2014-09-26 09:54:43 -0400

    Test Fix.
    Missing neko initialization

--- test/haxe/TestNull.hx
+++ test/haxe/TestNull.hx
@@ -7,6 +7,8 @@ class TestNull extends haxe.unit.TestCase
       {
         #if neko
 	var _test = neko.Lib.load("test","test_null",0);
+        var _init = neko.Lib.load("test","neko_init",5);
+        _init(function(s) return new String(s), function(len:Int) { var r = []; if (len > 0) r[len - 1] = null; return r; }, null, true, false);
         #else
 	var _test = cpp.Lib.load("test","test_null",0);
         #end
@@ -19,5 +21,4 @@ class TestNull extends haxe.unit.TestCase
       }
       assertEquals("", err);
    }
-
 }
