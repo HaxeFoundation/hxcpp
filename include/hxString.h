4741105a51bb2db0cddc93e743340c09f2b33e1e
hughsando <gamehaxe@gmail.com>
2014-12-09 23:18:46 +0800

    Disable precalculated hashes until I can work out why it fails sometimes on mac

--- include/hxString.h
+++ include/hxString.h
@@ -104,8 +104,8 @@ public:
    inline unsigned int hash( ) const
    {
       if (__s==0) return 0;
-      if ( (((unsigned int *)__s)[-1] & HX_GC_NO_HASH_MASK) == HX_GC_CONST_ALLOC_BIT)
-         return ((unsigned int *)__s)[-2];
+      //if ( (((unsigned int *)__s)[-1] & HX_GC_NO_HASH_MASK) == HX_GC_CONST_ALLOC_BIT)
+      //   return ((unsigned int *)__s)[-2];
 
       unsigned int result = 0;
       for(int i=0;i<length;i++)
