2444f40e5d15d56191b1e1fe42d4b0092237bf36
Joshua Granick <jgranick@users.noreply.github.com>
2015-01-22 14:49:07 -0800

    If a Build.xml references no files for the current target/architecture, skip the build instead of erroring

--- tools/hxcpp/BuildTool.hx
+++ tools/hxcpp/BuildTool.hx
@@ -133,6 +133,13 @@ class BuildTool
 
       parseXML(xml,"");
       popFile();
+      
+      if (mFileStack.length == 0) {
+         
+         Log.info("", "\n\x1b[33;1mNo files, skipping build\x1b[0m");
+         return;
+         
+      }
 
       include("toolchain/" + mDefines.get("toolchain") + "-toolchain.xml");
       
