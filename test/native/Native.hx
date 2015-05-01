d98ad89b9f4999daea99dc08d9b788b7feee0435
Hugh <gamehaxe@gmail.com>
2015-05-01 13:58:18 +0800

    Add some tests for @:nativeGen, @:unreflective and @:nonVirtual

--- test/native/Native.hx
+++ test/native/Native.hx
@@ -6,6 +6,8 @@ class Native
    {
 		var r = new haxe.unit.TestRunner();
 		r.add(new tests.TestRgb());
+		r.add(new tests.TestNativeGen());
+		r.add(new tests.TestNonVirtual());
       var t0 = haxe.Timer.stamp();
 		var success = r.run();
       trace(" Time : " + (haxe.Timer.stamp()-t0)*1000 );
