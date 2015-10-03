e4625baa6127f7a12d4ee5d660085d00773fcb80
Dominic Guana <organicchemistry_01@yahoo.com.ph>
2015-10-04 06:01:32 +0800

    Update BuildTool.hx

--- tools/hxcpp/BuildTool.hx
+++ tools/hxcpp/BuildTool.hx
@@ -1396,20 +1396,21 @@ class BuildTool
       }
       else if ( (new EReg("linux","i")).match(os) )
       {
+         set64(defines,m64);
+         // Cross-compile?
          if(defines.exists("windows"))
          {
             defines.set("toolchain","mingw");
-            defines.set("mingw","mingw");
             defines.set("xcompile","1");
             defines.set("BINDIR", m64 ? "Windows64":"Windows");
          }
          else
          {
-            set64(defines,m64);
             defines.set("toolchain","linux");
             defines.set("linux","linux");
             defines.set("BINDIR", m64 ? "Linux64":"Linux");
          }
+
       }
       else if ( (new EReg("mac","i")).match(os) )
       {
