dd54095bb0426c1c9cf90c32466be3b939910079
Hugh <gamehaxe@gmail.com>
2015-11-16 12:59:11 +0800

    Do not add ws2_32.lib when building a library

--- hxcpp/StaticStd.hx
+++ hxcpp/StaticStd.hx
@@ -4,7 +4,7 @@ package hxcpp;
 @:buildXml("
 <target id='haxe'>
   <lib name='${HXCPP}/lib/${BINDIR}/libstd${LIBEXTRA}${LIBEXT}'/>
-   <lib name='ws2_32.lib' if='windows'/>
+   <lib name='ws2_32.lib' if='windows' unless='static_link' />
 </target>
 ")
 @:keep class StaticStd
