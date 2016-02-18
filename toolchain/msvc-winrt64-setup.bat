05dc6a385c2ead37c402f111f6d804e8987d4409
Carlos Madrazo <madrazo@bamtang.com>
2016-02-18 12:13:43 -0500

    WinRT. Toolchain and setup adjustments
    I had to add a HXCPP_WINRT_MD flag since there seems to be no way to
    check the value of "ABI" (like, if ABI is "-MD", "-MT" or "-ZW"). This
    leaves the defult of winrt static compilation to ZW, but allows optional
    MD compilation more easily. Not sure if also works for non-static_link.
    
    Also I added HXCPP_HACK_PDBSRV to setup, to fix a compile error

--- toolchain/msvc-winrt64-setup.bat
+++ toolchain/msvc-winrt64-setup.bat
@@ -5,10 +5,9 @@ setlocal enabledelayedexpansion
 		@set "PATH=%ProgramFiles(x86)%\Windows Kits\10\bin\x64;!PATH!"
 		@set "LIB=%ProgramFiles(x86)%\Windows Kits\10\Lib\10.0.10240.0\um\x64;!LIB!"
 		@set "LIBPATH=%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\lib\store\references;!LIBPATH!"
-
-	@echo HXCPP_VARS
-	@set
-
+		@echo HXCPP_VARS
+		@set
+		@echo HXCPP_HACK_PDBSRV=1
 ) else (
 	echo Warning: Could not find x64 environment variables for Visual Studio 2015
 )
