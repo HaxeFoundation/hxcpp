a2d460e39812cb20067a5a188ca92643bb1f7ec1
Carlos Madrazo <madrazo@bamtang.com>
2016-02-25 15:28:32 -0500

    WinRT: Automatically get SDK version
    Get current SDK version from registry

--- toolchain/msvc-winrt-setup.bat
+++ toolchain/msvc-winrt-setup.bat
@@ -1,14 +1,19 @@
 setlocal enabledelayedexpansion
 @if exist "%VS140COMNTOOLS%\vsvars32.bat" (
-	@call "%VS140COMNTOOLS%\vsvars32.bat"
-		@set "INCLUDE=%ProgramFiles(x86)%\Windows Kits\10\Include;!INCLUDE!"
-		@set "PATH=%ProgramFiles(x86)%\Windows Kits\10\bin\x86;!PATH!"
-		@set "LIB=%ProgramFiles(x86)%\Windows Kits\10\Lib\10.0.10240.0\um\x86;!LIB!"
-		@set "LIBPATH=%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\lib\store\references;!LIBPATH!"
-
-	@echo HXCPP_VARS
-	@set
-
+        FOR /F "usebackq skip=2 tokens=1-3" %%A IN (`REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Microsoft SDKs\Windows" /v CurrentVersion 2^>nul`) DO (
+            set CurrentSDKVersion=%%C
+        )
+        if defined CurrentSDKVersion (
+            @call "%VS140COMNTOOLS%\vsvars32.bat"
+            @set "INCLUDE=%ProgramFiles(x86)%\Windows Kits\10\Include;!INCLUDE!"
+            @set "PATH=%ProgramFiles(x86)%\Windows Kits\10\bin\x86;!PATH!"
+            @set "LIB=%ProgramFiles(x86)%\Windows Kits\10\Lib\%CurrentSDKVersion%\um\x86;!LIB!"
+            @set "LIBPATH=%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\lib\store\references;!LIBPATH!"
+            @echo HXCPP_VARS
+            @set
+        ) else (
+            echo Warning: Could not find Windows SDK
+        )
 ) else (
 	echo Warning: Could not find environment variables for Visual Studio 2015
 )
--- toolchain/msvc-winrt64-setup.bat
+++ toolchain/msvc-winrt64-setup.bat
@@ -1,13 +1,20 @@
 setlocal enabledelayedexpansion
 @if exist "%VS140COMNTOOLS%\..\..\VC\bin\x86_amd64\vcvarsx86_amd64.bat" (
-    @call "%VS140COMNTOOLS%\..\..\VC\bin\x86_amd64\vcvarsx86_amd64.bat"
-		@set "INCLUDE=%ProgramFiles(x86)%\Windows Kits\10\Include;!INCLUDE!"
-		@set "PATH=%ProgramFiles(x86)%\Windows Kits\10\bin\x64;!PATH!"
-		@set "LIB=%ProgramFiles(x86)%\Windows Kits\10\Lib\10.0.10240.0\um\x64;!LIB!"
-		@set "LIBPATH=%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\lib\store\references;!LIBPATH!"
-		@echo HXCPP_VARS
-		@set
-		@echo HXCPP_HACK_PDBSRV=1
+        FOR /F "usebackq skip=2 tokens=1-3" %%A IN (`REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Microsoft SDKs\Windows" /v CurrentVersion 2^>nul`) DO (
+            set CurrentSDKVersion=%%C
+        )
+        if defined CurrentSDKVersion (    
+	    @call "%VS140COMNTOOLS%\..\..\VC\bin\x86_amd64\vcvarsx86_amd64.bat"
+            @set "INCLUDE=%ProgramFiles(x86)%\Windows Kits\10\Include;!INCLUDE!"
+            @set "PATH=%ProgramFiles(x86)%\Windows Kits\10\bin\x64;!PATH!"
+            @set "LIB=%ProgramFiles(x86)%\Windows Kits\10\Lib\%CurrentSDKVersion%\um\x64;!LIB!"
+            @set "LIBPATH=%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\lib\store\references;!LIBPATH!"
+            @echo HXCPP_VARS
+            @set
+            @echo HXCPP_HACK_PDBSRV=1
+        ) else (
+            echo Warning: Could not find Windows SDK
+        )
 ) else (
 	echo Warning: Could not find x64 environment variables for Visual Studio 2015
 )
