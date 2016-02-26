0da39ac4fd6d3f394f45222b8eae41b33235ff52
Carlos Madrazo <madrazo@bamtang.com>
2016-02-26 16:53:44 -0500

    WinrRT, revert automatically get SDK version but get Kits Path
    Actually, we cannot get the SDK version via registry, but we can get the
    Kits path. References:
    http://stackoverflow.com/questions/35119223/how-to-programatically-detect-and-locate-the-windows-10-sdk
    
    http://stackoverflow.com/questions/445167/how-can-i-get-the-value-of-a-registry-key-from-within-a-batch-script

--- toolchain/msvc-winrt64-setup.bat
+++ toolchain/msvc-winrt64-setup.bat
@@ -1,19 +1,23 @@
 setlocal enabledelayedexpansion
 @if exist "%VS140COMNTOOLS%\..\..\VC\bin\x86_amd64\vcvarsx86_amd64.bat" (
-        FOR /F "usebackq skip=2 tokens=1-3" %%A IN (`REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Microsoft SDKs\Windows" /v CurrentVersion 2^>nul`) DO (
-            set CurrentSDKVersion=%%C
+        FOR /F "usebackq skip=4 tokens=1-3" %%A IN (`REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows Kits\Installed Roots" /v KitsRoot10 2^>nul`) DO (
+            set KitsRoot10Path=%%C
         )
-        if defined CurrentSDKVersion (    
-	    @call "%VS140COMNTOOLS%\..\..\VC\bin\x86_amd64\vcvarsx86_amd64.bat"
-            @set "INCLUDE=%ProgramFiles(x86)%\Windows Kits\10\Include;!INCLUDE!"
-            @set "PATH=%ProgramFiles(x86)%\Windows Kits\10\bin\x64;!PATH!"
-            @set "LIB=%ProgramFiles(x86)%\Windows Kits\10\Lib\%CurrentSDKVersion%\um\x64;!LIB!"
+        @if defined KitsRoot10Path ( 
+            @call "%VS140COMNTOOLS%\..\..\VC\bin\x86_amd64\vcvarsx86_amd64.bat"
+            @set "INCLUDE=%KitsRoot10Path%Include;!INCLUDE!"
+            @set "PATH=%KitsRoot10Path%bin\x64;!PATH!"
+            @if exist "%KitsRoot10Path%Lib\10.0.10586.0" (
+                @set "LIB=%KitsRoot10Path%Lib\10.0.10586.0\um\x64;!LIB!"
+            ) else ( 
+                @set "LIB=%KitsRoot10Path%Lib\10.0.10240.0\um\x64;!LIB!"
+            )
             @set "LIBPATH=%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\lib\store\references;!LIBPATH!"
-            @echo HXCPP_VARS
+            '@echo HXCPP_VARS
             @set
             @echo HXCPP_HACK_PDBSRV=1
         ) else (
-            echo Warning: Could not find Windows SDK
+            echo Warning: Could not find Windows Kits Root path
         )
 ) else (
 	echo Warning: Could not find x64 environment variables for Visual Studio 2015
