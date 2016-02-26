b5982ad8b6430b52288f96ef75a7e47dfdff9211
Carlos Madrazo <madrazo@bamtang.com>
2016-02-26 17:49:48 -0500

    WinRT revert registry stuff
    Sorry. It is not working OK, so I am reverting the fancy stuff for now.
    I am just setting the SDK number to the latest 10.0.10586.0

--- toolchain/msvc-winrt-setup.bat
+++ toolchain/msvc-winrt-setup.bat
@@ -1,23 +1,12 @@
 setlocal enabledelayedexpansion
 @if exist "%VS140COMNTOOLS%\vsvars32.bat" (
-        FOR /F "usebackq skip=4 tokens=1-3" %%A IN (`REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows Kits\Installed Roots" /v KitsRoot10 2^>nul`) DO (
-            set KitsRoot10Path=%%C
-        )
-        if defined KitsRoot10Path (
-            @call "%VS140COMNTOOLS%\vsvars32.bat"
-            @set "INCLUDE=%KitsRoot10Path%Include;!INCLUDE!"
-            @set "PATH=%KitsRoot10Path%bin\x86;!PATH!"
-            @if exist "%KitsRoot10Path%Lib\10.0.10586.0" (
-                @set "LIB=%KitsRoot10Path%Lib\10.0.10586.0\um\x86;!LIB!"
-            ) else ( 
-                @set "LIB=%KitsRoot10Path%Lib\10.0.10240.0\um\x86;!LIB!"
-            )
-            @set "LIBPATH=%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\lib\store\references;!LIBPATH!"
-            @echo HXCPP_VARS
-            @set
-        ) else (
-            echo Warning: Could not find Windows 10 Kits Path
-        )
+        @call "%VS140COMNTOOLS%\vsvars32.bat"
+        @set "INCLUDE=%ProgramFiles(x86)%\Windows Kits\10\Include;!INCLUDE!"
+        @set "PATH=%ProgramFiles(x86)%\Windows Kits\10\bin\x86;!PATH!"
+        @set "LIB=%ProgramFiles(x86)%\Windows Kits\10\Lib\10.0.10586.0\um\x86;!LIB!"
+        @set "LIBPATH=%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\lib\store\references;!LIBPATH!"
+        @echo HXCPP_VARS
+        @set
 ) else (   
     echo Warning: Could not find environment variables for Visual Studio 2015
 )
--- toolchain/msvc-winrt64-setup.bat
+++ toolchain/msvc-winrt64-setup.bat
@@ -1,24 +1,13 @@
 setlocal enabledelayedexpansion
 @if exist "%VS140COMNTOOLS%\..\..\VC\bin\x86_amd64\vcvarsx86_amd64.bat" (
-        FOR /F "usebackq skip=4 tokens=1-3" %%A IN (`REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows Kits\Installed Roots" /v KitsRoot10 2^>nul`) DO (
-            set KitsRoot10Path=%%C
-        )
-        @if defined KitsRoot10Path ( 
-            @call "%VS140COMNTOOLS%\..\..\VC\bin\x86_amd64\vcvarsx86_amd64.bat"
-            @set "INCLUDE=%KitsRoot10Path%Include;!INCLUDE!"
-            @set "PATH=%KitsRoot10Path%bin\x64;!PATH!"
-            @if exist "%KitsRoot10Path%Lib\10.0.10586.0" (
-                @set "LIB=%KitsRoot10Path%Lib\10.0.10586.0\um\x64;!LIB!"
-            ) else ( 
-                @set "LIB=%KitsRoot10Path%Lib\10.0.10240.0\um\x64;!LIB!"
-            )
-            @set "LIBPATH=%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\lib\store\references;!LIBPATH!"
-            @echo HXCPP_VARS
-            @set
-            @echo HXCPP_HACK_PDBSRV=1
-        ) else (
-            echo Warning: Could not find Windows Kits Root path
-        )
+        @call "%VS140COMNTOOLS%\..\..\VC\bin\x86_amd64\vcvarsx86_amd64.bat"
+        @set "INCLUDE=%ProgramFiles(x86)%\Windows Kits\10\Include;!INCLUDE!"
+        @set "PATH=%ProgramFiles(x86)%\Windows Kits\10\bin\x64;!PATH!"
+        @set "LIB=%ProgramFiles(x86)%\Windows Kits\10\Lib\10.0.10586.0\um\x64;!LIB!"
+        @set "LIBPATH=%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\lib\store\references;!LIBPATH!"
+        @echo HXCPP_VARS
+        @set
+        @echo HXCPP_HACK_PDBSRV=1
 ) else (
 	echo Warning: Could not find x64 environment variables for Visual Studio 2015
 )
