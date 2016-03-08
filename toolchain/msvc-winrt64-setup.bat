53b7ccb1128fc99bd8101bcd0046791e09cede53
Carlos Madrazo <madrazo@bamtang.com>
2016-03-08 09:33:25 -0500

    Winrt toolchain fixes
    Remove HXCPP_WINRT_NATIVE_LIBS and set MD ABI the default when compiling
    with static linking.
    Using environment variables to correctly locate the Windows Kits
    directory and the Lib directoryof the latest SDK edition.

--- toolchain/msvc-winrt64-setup.bat
+++ toolchain/msvc-winrt64-setup.bat
@@ -1,13 +1,13 @@
 setlocal enabledelayedexpansion
 @if exist "%VS140COMNTOOLS%\..\..\VC\bin\x86_amd64\vcvarsx86_amd64.bat" (
-        @call "%VS140COMNTOOLS%\..\..\VC\bin\x86_amd64\vcvarsx86_amd64.bat"
-        @set "INCLUDE=%ProgramFiles(x86)%\Windows Kits\10\Include;!INCLUDE!"
-        @set "PATH=%ProgramFiles(x86)%\Windows Kits\10\bin\x64;!PATH!"
-        @set "LIB=%ProgramFiles(x86)%\Windows Kits\10\Lib\10.0.10586.0\um\x64;!LIB!"
-        @set "LIBPATH=%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\lib\store\references;!LIBPATH!"
-        @echo HXCPP_VARS
-        @set
-        @echo HXCPP_HACK_PDBSRV=1
+    @call "%VS140COMNTOOLS%\..\..\VC\bin\x86_amd64\vcvarsx86_amd64.bat"
+        @set "INCLUDE=%WindowsSdkDir%Include;!INCLUDE!"
+        @set "PATH=%WindowsSdkDir%bin\x64;!PATH!"
+        @set "LIB=%WindowsSdkDir%Lib\%WindowsSDKLibVersion%um\x86;!LIB!"
+        @set "LIBPATH=%VS140COMNTOOLS%\..\..\VC\lib\store\references;!LIBPATH!"
+    @echo HXCPP_VARS
+    @set
+    @echo HXCPP_HACK_PDBSRV=1
 ) else (
-	echo Warning: Could not find x64 environment variables for Visual Studio 2015
+    echo Warning: Could not find x64 environment variables for Visual Studio 2015
 )
