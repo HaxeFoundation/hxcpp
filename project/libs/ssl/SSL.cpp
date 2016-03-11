097221cabe816aa2e056695f0c3e7ae96791de0e
Pascal Peridont <pperidont@motion-twin.com>
2016-03-11 15:08:01 +0100

    Add support for certificate_load_defaults() on Mac

--- project/libs/ssl/SSL.cpp
+++ project/libs/ssl/SSL.cpp
@@ -9,11 +9,16 @@
 typedef int SOCKET;
 #endif
 
+
 #ifndef STATIC_LINK
 #define IMPLEMENT_API
 #endif
 
 #include <hx/CFFI.h>
+
+#if defined(NEKO_MAC) && !defined(IPHONE) && !defined(APPLETV)
+#include <Security/Security.h>
+#endif
 
 typedef size_t socket_int;
 
@@ -349,24 +354,71 @@ static value conf_set_servername_callback( value config, value cb ){
 }
 
 static value cert_load_defaults(){
-#ifdef NEKO_WINDOWS
+#if defined(NEKO_WINDOWS)
 	value v;
 	HCERTSTORE store;
 	PCCERT_CONTEXT cert;
-	mbedtls_x509_crt *chain = (mbedtls_x509_crt *)malloc(sizeof(mbedtls_x509_crt));
-	mbedtls_x509_crt_init( chain );
+	mbedtls_x509_crt *chain = NULL;
 	if( store = CertOpenSystemStore(0, (LPCSTR)"Root") ){
 		cert = NULL;
-		while( cert = CertEnumCertificatesInStore(store, cert) )
+		while( cert = CertEnumCertificatesInStore(store, cert) ){
+			if( chain == NULL ){
+				chain = (mbedtls_x509_crt *)malloc(sizeof(mbedtls_x509_crt));
+				mbedtls_x509_crt_init( chain );
+			}
 			mbedtls_x509_crt_parse_der( chain, (unsigned char *)cert->pbCertEncoded, cert->cbCertEncoded );
+		}
 		CertCloseStore(store, 0);
 	}
-	v = alloc_abstract(k_cert, chain);
-	val_gc(v,free_cert);
-	return v;
-#else
-	return val_null;
+	if( chain != NULL ){
+		v = alloc_abstract(k_cert, chain);
+		val_gc(v,free_cert);
+		return v;
+	}
+#elif defined(NEKO_MAC) && !defined(IPHONE) && !defined(APPLETV)
+	CFMutableDictionaryRef search;
+	CFArrayRef result;
+	SecKeychainRef keychain;
+	SecCertificateRef item;
+	CFDataRef dat;
+	value v;
+	mbedtls_x509_crt *chain = NULL;
+
+	// Load keychain
+	if( SecKeychainOpen("/System/Library/Keychains/SystemRootCertificates.keychain",&keychain) != errSecSuccess )
+		return val_null;
+
+	// Search for certificates
+	search = CFDictionaryCreateMutable( NULL, 0, NULL, NULL );
+	CFDictionarySetValue( search, kSecClass, kSecClassCertificate );
+	CFDictionarySetValue( search, kSecMatchLimit, kSecMatchLimitAll );
+	CFDictionarySetValue( search, kSecReturnRef, kCFBooleanTrue );
+	CFDictionarySetValue( search, kSecMatchSearchList, CFArrayCreate(NULL, (const void **)&keychain, 1, NULL) );
+	if( SecItemCopyMatching( search, (CFTypeRef *)&result ) == errSecSuccess ){
+		CFIndex n = CFArrayGetCount( result );
+		for( CFIndex i = 0; i < n; i++ ){
+			item = (SecCertificateRef)CFArrayGetValueAtIndex( result, i );
+
+			// Get certificate in DER format
+			dat = SecCertificateCopyData( item );
+			if( dat ){
+				if( chain == NULL ){
+					chain = (mbedtls_x509_crt *)malloc(sizeof(mbedtls_x509_crt));
+					mbedtls_x509_crt_init( chain );
+				}
+				mbedtls_x509_crt_parse_der( chain, (unsigned char *)CFDataGetBytePtr(dat), CFDataGetLength(dat) );
+				CFRelease( dat );
+			}
+		}
+	}
+	CFRelease(keychain);
+	if( chain != NULL ){
+		v = alloc_abstract(k_cert, chain);
+		val_gc(v,free_cert);
+		return v;
+	}
 #endif
+	return val_null;
 }
 
 static value cert_load_file(value file){
