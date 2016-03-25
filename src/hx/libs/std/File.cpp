d729fdd996f2793e7547f472c5d6d42f466e9314
Hugh <gamehaxe@gmail.com>
2016-03-25 17:32:54 +0800

    More work on file externs. Merge some new code from lib area

--- src/hx/libs/std/File.cpp
+++ src/hx/libs/std/File.cpp
@@ -1,6 +1,9 @@
 #include <hxcpp.h>
 #include <stdio.h>
 #include <string.h>
+#include <vector>
+
+#include <hx/OS.h>
 
 #ifdef NEKO_WINDOWS
 #   include <windows.h>
@@ -14,41 +17,6 @@
    </p>
    </doc>
 **/
-
-
-#if defined(ANDROID) || defined(IPHONE)
-typedef char FilenameChar;
-#define val_filename val_string
-#define alloc_filename alloc_string
-
-#define MAKE_STDIO(k) \
-   static value file_##k() { \
-      fio *f; \
-      f = new fio(#k,k); \
-      value result = alloc_abstract(k_file,f); \
-      val_gc(result,free_stdfile); \
-      return result; \
-   } \
-   DEFINE_PRIM(file_##k,0);
-
-
-#else
-typedef wchar_t FilenameChar;
-#define val_filename val_wstring
-#define alloc_filename alloc_wstring
-
-#define MAKE_STDIO(k) \
-   static value file_##k() { \
-      fio *f; \
-      f = new fio(L###k,k); \
-      value result = alloc_abstract(k_file,f); \
-      val_gc(result,free_stdfile); \
-      return result; \
-   } \
-   DEFINE_PRIM(file_##k,0);
-
-
-#endif
 
 namespace
 {
@@ -92,17 +60,16 @@ struct fio : public hx::Object
 
 fio *getFio(Dynamic handle, bool inRequireFile=true)
 {
-   fio *result = dynamic_cast<fio *>(handle);
+   fio *result = dynamic_cast<fio *>(handle.mPtr);
    if (!result || (!result->io && inRequireFile))
       hx::Throw( HX_CSTRING("Bad file handle") );
    return result;
 }
 
 
-static void file_error(const char *msg, String inName, bool inExitZone)
+static void file_error(const char *msg, String inName)
 {
-   if (inExitZone)
-      hx::ExitGCFreeZone();
+   hx::ExitGCFreeZone();
    Array<String> err = Array_obj<String>::__new(2,2);
    err[0] = String(msg);
    err[1] = inName;
@@ -121,10 +88,10 @@ static void file_error(const char *msg, String inName, bool inExitZone)
 Dynamic _hx_std_file_open( String fname, String r )
 {
    hx::EnterGCFreeZone();
-   File *file = fopen(fname.__s,r.__s);
-   hx::ExitGCFreeZone();
+   FILE *file = fopen(fname.__s,r.__s);
    if (!file)
-      file_error("file_open",fname,false);
+      file_error("file_open",fname);
+   hx::ExitGCFreeZone();
 
    fio *f = new fio;
    f->create(file,fname,true);
@@ -135,10 +102,10 @@ Dynamic _hx_std_file_open( String fname, String r )
    file_close : 'file -> void
    <doc>Close an file. Any other operations on this file will fail</doc> 
 **/
-void _hx_std_file_close( Dyamic handle )
+void _hx_std_file_close( Dynamic handle )
 {
    fio *fio = getFio(handle);
-   fio->destroy();
+   fio->destroy(true);
 }
 
 /**
@@ -158,31 +125,24 @@ String hx_std_file_name( Dynamic handle )
    Returns the number of chars written which is >= 0.
    </doc>
 **/
-value file_write( value o, value s, value pp, value n )
+int _hx_std_file_write( Dynamic handle, Array<unsigned char> s, int p, int n )
 {
-   int p, len;
-   int buflen;
-   fio *f;
-   val_check_kind(o,k_file);
-   val_check(s,buffer);
-   buffer buf = val_to_buffer(s);
-   buflen = buffer_size(buf);
-   val_check(pp,int);
-   val_check(n,int);
-   f = val_file(o);
-   p = val_int(pp);
-   len = val_int(n);
+   fio *f = getFio(handle);
+
+   int buflen = s->length;
+   int len = n;
    if( p < 0 || len < 0 || p > buflen || p + len > buflen )
-      return alloc_null();
+      return 0;
 
    hx::EnterGCFreeZone();
-   while( len > 0 ) {
-      int d;
+   while( len > 0 )
+   {
       POSIX_LABEL(file_write_again);
-      d = (int)fwrite(buffer_data(buf)+p,1,len,f->io);
-      if( d <= 0 ) {
+      int d = (int)fwrite(&s[p],1,len,f->io);
+      if( d <= 0 )
+      {
          HANDLE_FINTR(f->io,file_write_again);
-         file_error("file_write",f);
+         file_error("file_write",f->name);
       }
       p += d;
       len -= d;
@@ -198,34 +158,30 @@ value file_write( value o, value s, value pp, value n )
    Returns the number of chars readed which is > 0 (or 0 if l == 0).
    </doc>
 **/
-static value file_read( value o, value s, value pp, value n ) {
-   fio *f;
-   int p;
-   int len;
-   int buf_len;
-   val_check_kind(o,k_file);
-   val_check(s,buffer);
-   buffer buf = val_to_buffer(s);
-   buf_len = buffer_size(buf);
-   val_check(pp,int);
-   val_check(n,int);
-   f = val_file(o);
-   p = val_int(pp);
-   len = val_int(n);
+int hx_std_file_read( Dynamic handle, Array<unsigned char> buf, int p, int n )
+{
+   fio *f = getFio(handle);
+
+   int buf_len = buf->length;
+   int len = n;
    if( p < 0 || len < 0 || p > buf_len || p + len > buf_len )
-      return alloc_null();
+      return 0;
+
    hx::EnterGCFreeZone();
-   while( len > 0 ) {
-      int d;
+   // Attempt to increase the chances of pinning on the stack...
+   unsigned char *bufPtr = &buf[0];
+   while( len > 0 )
+   {
       POSIX_LABEL(file_read_again);
-      d = (int)fread(buffer_data(buf)+p,1,len,f->io);
-      if( d <= 0 ) {
-         int size = val_int(n) - len;
+      int d = (int)fread(bufPtr + p,1,len,f->io);
+      if( d <= 0 )
+      {
+         int size = n - len;
          HANDLE_FINTR(f->io,file_read_again);
          if( size == 0 )
-            file_error("file_read",f);
+            file_error("file_read",f->name);
          hx::ExitGCFreeZone();
-         return alloc_int(size);
+         return size;
       }
       p += d;
       len -= d;
@@ -238,155 +194,150 @@ static value file_read( value o, value s, value pp, value n ) {
    file_write_char : 'file -> c:int -> void
    <doc>Write the char [c]. Error if [c] outside of the range 0..255</doc>
 **/
-static value file_write_char( value o, value c ) {
-   unsigned char cc;
-   fio *f;
-   val_check(c,int);
-   val_check_kind(o,k_file);
-   if( val_int(c) < 0 || val_int(c) > 255 )
-      return alloc_null();
-   cc = (char)val_int(c);
-   f = val_file(o);
+void _hx_std_file_write_char( Dynamic handle, int c )
+{
+   fio *f = getFio(handle);
+   if( c < 0 || c > 255 )
+      return;
+   char cc = (char)c;
+
    hx::EnterGCFreeZone();
    POSIX_LABEL(write_char_again);
-   if( fwrite(&cc,1,1,f->io) != 1 ) {
+   if( fwrite(&cc,1,1,f->io) != 1 )
+   {
       HANDLE_FINTR(f->io,write_char_again);
-      file_error("file_write_char",f);
+      file_error("file_write_char",f->name);
    }
    hx::ExitGCFreeZone();
-   return alloc_bool(true);
 }
 
 /**
    file_read_char : 'file -> int
    <doc>Read a char from the file. Exception on error</doc>
 **/
-static value file_read_char( value o ) {
-   unsigned char cc;
-   fio *f;
-   val_check_kind(o,k_file);
-   f = val_file(o);
+int _hx_std_file_read_char( Dynamic handle )
+{
+   fio *f = getFio(handle);
+
+   unsigned char cc = 0;
    hx::EnterGCFreeZone();
    POSIX_LABEL(read_char_again);
-   if( fread(&cc,1,1,f->io) != 1 ) {
+   if( fread(&cc,1,1,f->io) != 1 )
+   {
       HANDLE_FINTR(f->io,read_char_again);
-      file_error("file_read_char",f);
+      file_error("file_read_char",f->name);
    }
    hx::ExitGCFreeZone();
-   return alloc_int(cc);
+   return cc;
 }
 
 /**
    file_seek : 'file -> pos:int -> mode:int -> void
    <doc>Use [fseek] to move the file pointer.</doc>
 **/
-static value file_seek( value o, value pos, value kind ) {
-   fio *f;
-   val_check_kind(o,k_file);
-   val_check(pos,int);
-   val_check(kind,int);
-   f = val_file(o);
+void _hx_std_file_seek( Dynamic handle, int pos, int kind )
+{
+   fio *f = getFio(handle);
    hx::EnterGCFreeZone();
-   if( fseek(f->io,val_int(pos),val_int(kind)) != 0 )
-      file_error("file_seek",f);
+   if( fseek(f->io,pos,kind) != 0 )
+      file_error("file_seek",f->name);
    hx::ExitGCFreeZone();
-   return alloc_bool(true);
 }
 
 /**
    file_tell : 'file -> int
    <doc>Return the current position in the file</doc>
 **/
-static value file_tell( value o ) {
-   int p;
-   fio *f;
-   val_check_kind(o,k_file);
-   f = val_file(o);
+int _hx_std_file_tell( Dynamic handle )
+{
+   fio *f = getFio(handle);
    hx::EnterGCFreeZone();
-   p = ftell(f->io);
+   int p = ftell(f->io);
    if( p == -1 )
-      file_error("file_tell",f);
+      file_error("file_tell",f->name);
    hx::ExitGCFreeZone();
-   return alloc_int(p);
+   return p;
 }
 
 /**
    file_eof : 'file -> bool
    <doc>Tell if we have reached the end of the file</doc>
 **/
-static value file_eof( value o ) {
-   val_check_kind(o,k_file);
-   return alloc_bool( feof(val_file(o)->io) );
+bool _hx_std_file_eof( Dynamic handle )
+{
+   fio *f = getFio(handle);
+   return feof(f->io);
 }
 
 /**
    file_flush : 'file -> void
    <doc>Flush the file buffer</doc>
 **/
-static value file_flush( value o ) {
-   fio *f;
-   val_check_kind(o,k_file);
-   f = val_file(o);
+static void _hx_std_file_flush( Dynamic handle )
+{
+   fio *f = getFio(handle);
    hx::EnterGCFreeZone();
    if( fflush( f->io ) != 0 )
-      file_error("file_flush",f);
+      file_error("file_flush",f->name);
    hx::ExitGCFreeZone();
-   return alloc_bool(true);
 }
 
 /**
    file_contents : f:string -> string
    <doc>Read the content of the file [f] and return it.</doc>
 **/
-static value file_contents( value name ) {
-   buffer s=0;
-   int len;
-   int p;
-   val_check(name,string);
-   fio f(val_filename(name));
-        const char *fname = val_string(name);
-        hx::EnterGCFreeZone();
-   f.io = fopen(fname,"rb");
-   if( f.io == NULL )
-      file_error("file_contents",&f);
-   fseek(f.io,0,SEEK_END);
-   len = ftell(f.io);
-   fseek(f.io,0,SEEK_SET);
-   hx::ExitGCFreeZone();
-   s = alloc_buffer_len(len);
-   p = 0;
+String _hx_std_file_contents( String name )
+{
+   std::vector<char> buffer;
+
    hx::EnterGCFreeZone();
-   while( len > 0 ) {
-      int d;
+   FILE *file = fopen(name.__s, "rb");
+   if(!file)
+      file_error("file_contents",name);
+
+   fseek(file,0,SEEK_END);
+   int len = ftell(file);
+   fseek(file,0,SEEK_SET);
+   buffer.resize(len);
+   int p = 0;
+   while( len > 0 )
+   {
       POSIX_LABEL(file_contents);
-      d = (int)fread((char*)buffer_data(s)+p,1,len,f.io);
-      if( d <= 0 ) {
-         HANDLE_FINTR(f.io,file_contents);
-         fclose(f.io);
-         file_error("file_contents",&f);
+      int d = (int)fread(&buffer[p],1,len,file);
+      if( d <= 0 )
+      {
+         HANDLE_FINTR(file,file_contents);
+         fclose(file);
+         file_error("file_contents",name);
       }
       p += d;
       len -= d;
-   }   
-   fclose(f.io);
-   hx::ExitGCFreeZone();
-   return buffer_val(s);
+   }
+   hx::EnterGCFreeZone();
+   fclose(file);
+   return String(&buffer[0], buffer.size()).dup();
 }
 
 
-/**
-   file_stdin : void -> 'file
-   <doc>The standard input</doc>
-**/
-MAKE_STDIO(stdin);
-/**
-   file_stdout : void -> 'file
-   <doc>The standard output</doc>
-**/
-MAKE_STDIO(stdout);
-/**
-   file_stderr : void -> 'file
-   <doc>The standard error output</doc>
-**/
-MAKE_STDIO(stderr);
+Dynamic _hx_std_file_stdin()
+{
+   fio *f = new fio();
+   f->create(stdin, HX_CSTRING("stdin"), false);
+   return f;
+}
+
+
+Dynamic _hx_std_file_stdout()
+{
+   fio *f = new fio();
+   f->create(stdout, HX_CSTRING("stdout"), false);
+   return f;
+}
+
+Dynamic _hx_std_file_stderr()
+{
+   fio *f = new fio();
+   f->create(stdout, HX_CSTRING("stderr"), false);
+   return f;
+}
 
