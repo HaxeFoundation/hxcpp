87841687e2964cc3a91166117cbcccbb014a6931
hughsando <gamehaxe@gmail.com>
2016-03-31 22:13:43 +0800

    Restore haxe 3.2.1 compatibility

--- src/hx/CFFI.cpp
+++ src/hx/CFFI.cpp
@@ -400,6 +400,10 @@ void val_array_set_i(hx::Object * arg1,int arg2,hx::Object *inVal)
 
 void val_array_set_size(hx::Object * arg1,int inLen)
 {
+   #if (HXCPP_API_LEVEL<330)
+   if (arg1==0) return;
+   arg1->__SetSize(inLen);
+   #else
    hx::ArrayBase *base = dynamic_cast<hx::ArrayBase *>(arg1);
    if (base)
    {
@@ -411,6 +415,7 @@ void val_array_set_size(hx::Object * arg1,int inLen)
       if (va)
          va->__SetSize(inLen);
    }
+   #endif
 }
 
 void val_array_push(hx::Object * arg1,hx::Object *inValue)
