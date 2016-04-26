89186ba5088e4a2212b9b276b5049d61567ac1ca
hughsando <gamehaxe@gmail.com>
2016-04-26 22:30:27 +0800

    Use 'cpp.Stdlib' for malloc/free

--- test/native/tests/TestPtr.hx
+++ test/native/tests/TestPtr.hx
@@ -1,6 +1,8 @@
 package tests;
 
 import NativeGen;
+import cpp.Stdlib;
+import cpp.Pointer;
 
 @:unreflective
 @:native("CVec")
@@ -18,15 +20,19 @@ typedef struct CVec{
 } CVec;')
 class TestPtr extends haxe.unit.TestCase{
 	
+   /*
+    Alternate version
 	@:generic
 	public static inline function malloc<T>(size:Int) : cpp.Pointer<T>{
 		var p : cpp.RawPointer<cpp.Void> = untyped __cpp__("malloc({0})",size);
-		return cast cpp.Pointer.fromRaw<T>( cast p );
+		return cast cpp.Pointer.fromRaw( cast p );
 	}
+   */
 	
     public function testMalloc() {
-		var a : cpp.Pointer<Vec> = malloc(3 * 4);
+		var a : Pointer<Vec> = Stdlib.malloc( Stdlib.sizeof(Vec) );
 		a.ptr.x = 66;
 		assertTrue( a.ptr.x == 66 );
+      Stdlib.free(a);
 	}
 }
