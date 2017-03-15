c8453cd91eb48c0540aef8c549ceb13870962373
Jeff <jeff.ward@gmail.com>
2017-03-15 09:28:59 -0600

    Cleanup whitespace formatting

--- test/telemetry/TestMain.hx
+++ test/telemetry/TestMain.hx
@@ -2,12 +2,12 @@ package;
 
 class TestMain {
 
-	static function main(){
-		var r = new haxe.unit.TestRunner();
-		r.add(new TestBasic());
+  static function main(){
+    var r = new haxe.unit.TestRunner();
+    r.add(new TestBasic());
     var t0 = haxe.Timer.stamp();
-		var success = r.run();
+    var success = r.run();
     trace(" Time : " + (haxe.Timer.stamp()-t0)*1000 );
-		Sys.exit(success ? 0 : 1);
-	}
+    Sys.exit(success ? 0 : 1);
+  }
 }
