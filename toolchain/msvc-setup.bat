2b71a87fe838b960f76c6ad4295537ea0d6a4582
Mike Smullin <mike.smullin@wildworks.com>
2017-06-05 12:48:23 -0600

    Microsoft Visual Studio 2017 team moved around the files they tried to throw us off the scent but they forgot the haxe community--we're like bloodhounds

--- toolchain/msvc-setup.bat
+++ toolchain/msvc-setup.bat
@@ -1,5 +1,9 @@
 setlocal enabledelayedexpansion
-@if exist "%HXCPP_MSVC%\vsvars32.bat" (
+@if exist "%HXCPP_MSVC%\vcvars32.bat" (
+	@call "%HXCPP_MSVC%\vcvars32.bat"
+	@echo HXCPP_VARS
+	@set
+) else if exist "%HXCPP_MSVC%\vsvars32.bat" (
 	@call "%HXCPP_MSVC%\vsvars32.bat"
 	@echo HXCPP_VARS
 	@set
