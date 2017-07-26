863ad5449d100fa80b8f08bb294f133edb49b872
Andy Li <andy@onthewings.net>
2017-07-26 16:29:49 +0800

    [TravisCI] fix mac builds

--- tools/haxe/build_osx.sh
+++ tools/haxe/build_osx.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
   set -ev
 
-  apt-get install ocaml
+  git clone --recursive https://github.com/HaxeFoundation/haxe.git ~/haxe --depth 1
+
+  brew tap Homebrew/bundle
+  brew bundle --file=~/haxe/tests/Brewfile
 
   export OPAMYES=1
   opam init
-  opam install camlp4 sedlex ocamlfind camlzip xml-light extlib rope ptmap
   eval `opam config env`
+  opam install camlp4 sedlex ocamlfind camlzip xml-light extlib rope
+
+  # Install a patched ptmap. See https://github.com/backtracking/ptmap/issues/3
+  opam install obuild qtest
+  git clone https://github.com/andyli/ptmap.git --branch 4.05 ~/ptmap
+  pushd ~/ptmap
+  make && sudo make install
+  popd
 
   brew install neko --HEAD;
 
   # Build haxe
-  git clone --recursive https://github.com/HaxeFoundation/haxe.git ~/haxe --depth 1
   pushd ~/haxe
   make ADD_REVISION=1 && sudo make install INSTALL_DIR=/usr/local
   popd
