3179be17f1a1a0b3d1cd57326c82df188c1ad15d
Thomas Uster <thomasuster@gmail.com>
2017-07-28 13:45:11 -0700

    Compile in CPP, but use neko for tool itself.

---
+++ tools/hxcpp/BuildToolLauncher.hx
@@ -0,0 +1,34 @@
+import sys.FileSystem;
+
+class BuildToolLauncher 
+{
+    public static function main():Void {
+        var bin:String = './bin/tools/${calcBinName()}';
+        if(FileSystem.exists(bin) && !isBuildingSelf()) {
+            Sys.exit(Sys.command('./bin/tools/${calcBinName()}', Sys.args()));
+        }
+        else {
+            Sys.exit(Sys.command('neko', ['hxcpp-fallback.n'].concat(Sys.args())));
+        }
+    }
+    
+    static function isBuildingSelf():Bool {
+        var args:Array<String> = Sys.args();
+        var last:String = args[args.length-1]; 
+        if(sys.FileSystem.exists('${last}../../haxelib.json')) {
+            
+            if(sys.io.File.getContent('${last}../../haxelib.json') == haxe.Resource.getString("Signature")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static function calcBinName():String {
+        #if windows
+        return 'BuildTool-debug.exe';
+        #else
+        return 'BuildTool-debug';
+        #end
+    }
+}
