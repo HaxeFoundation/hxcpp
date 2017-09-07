99d09bfe87db5257c69b92b0b5e3bf14d8d57fe1
Thomas Uster <thomasuster@gmail.com>
2017-09-06 22:26:17 -0700

    Added stress test that fails

--- test/haxe/TestMain.hx
+++ test/haxe/TestMain.hx
@@ -1,24 +1,22 @@
-package;
-import gc.TestGC;
-
 class TestMain {
 
-	static function main(){
-		var r = new haxe.unit.TestRunner();
-		r.add(new TestTypes());
-		r.add(new TestKeywords());
-		r.add(new TestSort());
-		r.add(new TestGC());
-		r.add(new TestIntHash());
-		r.add(new TestStringHash());
-		r.add(new TestObjectHash());
-		r.add(new TestWeakHash());
+   static function main() {
+      var r = new haxe.unit.TestRunner();
+      r.add(new TestTypes());
+      r.add(new TestKeywords());
+      r.add(new TestSort());
+      r.add(new gc.TestGC());
+      r.add(new gc.TestGCWithSys());
+      r.add(new TestIntHash());
+      r.add(new TestStringHash());
+      r.add(new TestObjectHash());
+      r.add(new TestWeakHash());
       #if cpp
-		r.add(new native.TestFinalizer());
+      r.add(new native.TestFinalizer());
       #end
       var t0 = haxe.Timer.stamp();
-		var success = r.run();
-      trace(" Time : " + (haxe.Timer.stamp()-t0)*1000 );
-		Sys.exit(success ? 0 : 1);
-	}
+      var success = r.run();
+      Sys.println("Time : " + (haxe.Timer.stamp()-t0)*1000 + " ms");
+      Sys.exit(success ? 0 : 1);
+   }
 }
