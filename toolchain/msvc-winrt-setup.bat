4edb1db80997e1ce5059894afc2bd24d405d3018
Carlos Madrazo <madrazo@bamtang.com>
2017-10-05 15:03:49 -0500

    Fix Winrt compile VS2017
    Added LIB and LIBPATH

--- toolchain/msvc-winrt-setup.bat
+++ toolchain/msvc-winrt-setup.bat
@@ -1,19 +1,17 @@
 setlocal enabledelayedexpansion
-@if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere" (
+@if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" (
 	for /f "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
 		@set InstallDir=%%i
 	)
 	@if exist "!InstallDir!\Common7\Tools\VsDevCmd.bat" (
 		@call "!InstallDir!\Common7\Tools\VsDevCmd.bat" -arch=x86 -app_platform=UWP -no_logo
+	        @set "LIB=%VCTOOLSINSTALLDIR%lib\x86\store;%WindowsSdkDir%Lib\%WindowsSDKLibVersion%um\x86;!LIB!"
+	        @set "LIBPATH=%VCTOOLSINSTALLDIR%lib\x86\store\references;!LIBPATH!"
 		@echo HXCPP_VARS
 		@set
 	) else (
 		echo Warning: Could not find Visual Studio 2017 VsDevCmd
 	)
-) else if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat" (
-	@call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat" -arch=x86 -app_platform=UWP -no_logo
-	@echo HXCPP_VARS
-	@set
 ) else if exist "%VS140COMNTOOLS%\vsvars32.bat" (
 	@call "%VS140COMNTOOLS%\vsvars32.bat"
 	@set "INCLUDE=%WindowsSdkDir%Include;!INCLUDE!"
