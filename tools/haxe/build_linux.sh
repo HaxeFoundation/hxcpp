49cf1e7804b6bc79954246bdec4ba277b97002de
Steve Mayhew <mayhew.steve@gmail.com>
2018-10-29 10:29:50 -0700

    Add ability to specify haxe version

--- tools/haxe/build_linux.sh
+++ tools/haxe/build_linux.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
   set -ev
 
-  git clone --recursive https://github.com/HaxeFoundation/haxe.git ~/haxe --depth 1
+  HAXE_REPO=${1-:"https://github.com/HaxeFoundation/haxe.git"}
+  HAXE_VERSION=${2-:"HEAD"}
+  git clone --branch ${HAXE_VERSION} --recursive ${HAXE_REPO} ~/haxe --depth 1
 
   sudo add-apt-repository ppa:haxe/ocaml -y
   sudo add-apt-repository ppa:haxe/snapshots -y
@@ -30,3 +32,4 @@
   mkdir ~/haxelib && haxelib setup ~/haxelib
   haxelib dev hxcpp $TRAVIS_BUILD_DIR
   haxelib install record-macros
+
