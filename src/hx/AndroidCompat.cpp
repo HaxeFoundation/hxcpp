c67c493ce461f79098c64ef50037200dcdb74557
Jean-Denis Boivin <jeandenis.boivin@gmail.com>
2019-06-04 23:02:59 -0400

    Attempt to fix crtbegin error

--- src/hx/AndroidCompat.cpp
+++ src/hx/AndroidCompat.cpp
@@ -9,7 +9,7 @@
 // These functions are inlined prior to android-ndk-platform-21, which means they
 // are missing from the libc functions on those phones, and you will get link errors.
 
-#if (HXCPP_ANDROID_PLATFORM>=21) && !defined(HXCPP_ARM64)
+#if (HXCPP_ANDROID_PLATFORM>=21) && !defined(HXCPP_ARM64) && !defined(HXCPP_ARMV7)
 extern "C" {
 
 
