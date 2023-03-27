27a962fdfa391095970ce4b492fbd6b659ea2fdc
Apprentice-Alchemist <53486764+Apprentice-Alchemist@users.noreply.github.com>
2023-03-27 20:49:24 +0200

    Make _hx_std_process_kill work everywhere.

--- src/hx/libs/std/Process.cpp
+++ src/hx/libs/std/Process.cpp
@@ -10,6 +10,7 @@
 #   include <unistd.h>
 #   include <memory.h>
 #   include <errno.h>
+#   include <signal.h>
 #   if defined(ANDROID) || defined(BLACKBERRY) || defined(EMSCRIPTEN)
 #      include <sys/wait.h>
 #   elif !defined(NEKO_MAC)
@@ -563,7 +564,7 @@ void _hx_std_process_kill( Dynamic handle )
 
    #ifdef NEKO_WINDOWS
    TerminateProcess(p->pinf.hProcess, -1);
-   #elif defined(APPLETV) && !defined(HX_APPLEWATCH)
+   #else
    kill(p->pid, SIGTERM);
    #endif
 }
