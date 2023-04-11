3ad136d3f21dac2f0ce2baf3adb53ed5186c0fcd
Aidan Lee <aidan.lee63@gmail.com>
2023-04-11 14:40:50 +0100

    Chunked stream writer

--- src/hx/libs/asys/libuv/stream/StreamWriter.cpp
+++ src/hx/libs/asys/libuv/stream/StreamWriter.cpp
@@ -1,40 +1,107 @@
 #include <hxcpp.h>
 #include "StreamWriter.h"
 
-hx::asys::libuv::stream::StreamWriter::StreamWriter(uv_stream_t* _stream) : stream(_stream) {}
-
-void hx::asys::libuv::stream::StreamWriter::write(Array<uint8_t> input, int offset, int length, Dynamic cbSuccess, Dynamic cbFailure)
+namespace
 {
-    auto staging = std::vector<char>(length);
-    auto buffer  = uv_buf_init(staging.data(), staging.size());
-
-    std::memcpy(staging.data(), input->getBase() + offset, length);
-
-    auto request = std::make_unique<uv_write_t>();
-    auto wrapper = [](uv_write_t* request, int status) {
-        auto gcZone = hx::AutoGCZone();
+    void onWriteCallback(uv_write_t* request, int result)
+    {
+        auto gcZone    = hx::AutoGCZone();
         auto spRequest = std::unique_ptr<uv_write_t>(request);
-        auto spData = std::unique_ptr<hx::asys::libuv::BaseRequest>(static_cast<hx::asys::libuv::BaseRequest*>(request->data));
+        auto writer    = static_cast<hx::asys::libuv::stream::StreamWriter*>(spRequest->data);
 
-        if (status < 0)
+        if (result < 0)
         {
-            Dynamic(spData->cbFailure.rooted)(hx::asys::libuv::uv_err_to_enum(status));
+            auto front     = writer->popFront();
+            auto cbFailure = Dynamic(front->cbFailure.rooted);
+
+            cbFailure(hx::asys::libuv::uv_err_to_enum(result));
+
+            return;
+        }
+
+        auto& front = writer->queue.front();
+
+        front->progress += front->staging.size();
+
+        if (front->progress < front->length)
+        {
+            writer->consume();
         }
         else
         {
-            Dynamic(spData->cbSuccess.rooted)();
+            auto f = writer->popFront();
+
+            writer->consume();
+
+            Dynamic(f->cbSuccess.rooted)(f->length);
         }
-    };
+    }
+}
 
-    auto result = uv_write(request.get(), stream, &buffer, 1, wrapper);
+hx::asys::libuv::stream::QueuedWrite::QueuedWrite(Array<uint8_t> _array, int _offset, int _length, Dynamic _cbSuccess, Dynamic _cbFailure)
+    : BaseRequest(_cbSuccess, _cbFailure)
+    , array(_array.mPtr)
+    , offset(_offset)
+    , length(_length)
+    , progress(0)
+    , staging(std::vector<char>()) {}
 
+hx::asys::libuv::stream::StreamWriter::StreamWriter(uv_stream_t* _stream)
+    : stream(_stream)
+    , queue(std::deque<std::unique_ptr<QueuedWrite>>()) {}
+
+void hx::asys::libuv::stream::StreamWriter::write(Array<uint8_t> input, int offset, int length, Dynamic cbSuccess, Dynamic cbFailure)
+{
+    auto startConsuming = queue.empty();
+
+    queue.push_back(std::make_unique<QueuedWrite>(input, offset, length, cbSuccess, cbFailure));
+
+    if (startConsuming)
+    {
+        consume();
+    }
+}
+
+void hx::asys::libuv::stream::StreamWriter::flush(Dynamic cbSuccess, Dynamic cbFailure)
+{
+    //
+}
+
+std::unique_ptr<hx::asys::libuv::stream::QueuedWrite> hx::asys::libuv::stream::StreamWriter::popFront()
+{
+    auto front = std::unique_ptr<hx::asys::libuv::stream::QueuedWrite>(std::move(queue.front()));
+
+    queue.pop_front();
+
+    return front;
+}
+
+void hx::asys::libuv::stream::StreamWriter::consume()
+{
+    if (queue.empty())
+    {
+        return;
+    }
+
+    auto& front   = queue.front();
+    auto  request = std::make_unique<uv_write_t>();
+    auto  size    = std::min(CHUNK_SIZE, front->length - front->progress);
+
+    front->staging.resize(size);
+
+    auto buffer = uv_buf_init(front->staging.data(), size);
+
+    std::memcpy(front->staging.data(), front->array.rooted->GetBase() + front->offset + front->progress, size);
+
+    auto result = uv_write(request.get(), stream, &buffer, 1, onWriteCallback);
     if (result < 0)
     {
-        cbFailure(hx::asys::libuv::uv_err_to_enum(result));
+        Dynamic(front->cbFailure.rooted)(uv_err_to_enum(result));
+
+        queue.pop_front();
+
+        return;
     }
-    else
-    {
-        request->data = new hx::asys::libuv::BaseRequest(cbSuccess, cbFailure);
-        request.release();
-    }
+
+    request.release()->data = this;
 }
--- src/hx/libs/asys/libuv/stream/StreamWriter.h
+++ src/hx/libs/asys/libuv/stream/StreamWriter.h
@@ -7,13 +7,33 @@
 
 namespace hx::asys::libuv::stream
 {
+    struct QueuedWrite final : public BaseRequest
+    {
+        const hx::RootedObject<Array_obj<uint8_t>> array;
+        const int offset;
+        const int length;
+        int progress;
+        std::vector<char> staging;
+
+        QueuedWrite(Array<uint8_t> _array, int _offset, int _length, Dynamic _cbSuccess, Dynamic _cbFailure);
+    };
+
     class StreamWriter final
     {
-    private:
-        uv_stream_t* const stream;
     public:
+        const int CHUNK_SIZE = static_cast<int>(std::numeric_limits<uint16_t>::max());
+
+        uv_stream_t* const stream;
+        std::deque<std::unique_ptr<QueuedWrite>> queue;
+
         StreamWriter(uv_stream_t* _stream);
 
         void write(Array<uint8_t> input, int offset, int length, Dynamic cbSuccess, Dynamic cbFailure);
+
+        void flush(Dynamic cbSuccess, Dynamic cbFailure);
+
+        std::unique_ptr<QueuedWrite> popFront();
+
+        void consume();
     };
 }
