78bf9e8fafb88d344f3b6e2be0fc147d49f59964
Aidan Lee <aidan.lee63@gmail.com>
2024-03-30 20:34:34 +0000

    Correctly cleanup child process context

--- src/hx/libs/asys/libuv/system/LibuvChildProcess.cpp
+++ src/hx/libs/asys/libuv/system/LibuvChildProcess.cpp
@@ -101,7 +101,9 @@ void hx::asys::libuv::system::LibuvChildProcess::exitCode(Dynamic cbSuccess, Dyn
 
 void hx::asys::libuv::system::LibuvChildProcess::close(Dynamic cbSuccess, Dynamic cbFailure)
 {
-	uv_close(reinterpret_cast<uv_handle_t*>(&ctx->request), hx::asys::libuv::clean_handle);
+	uv_close(reinterpret_cast<uv_handle_t*>(&ctx->request), [](uv_handle_t* handle) {
+		delete reinterpret_cast<hx::asys::libuv::system::LibuvChildProcess::Ctx*>(handle->data);
+	});
 
 	cbSuccess();
 }
