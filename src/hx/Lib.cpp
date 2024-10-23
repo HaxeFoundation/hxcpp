9a6c0b4ed5779fa7f8ca40d2ee08029dc569b2cc
Lily <egzozu.be.bas@gmail.com>
2024-10-23 21:12:46 +0300

    Update Lib.cpp

--- src/hx/Lib.cpp
+++ src/hx/Lib.cpp
@@ -393,7 +393,11 @@ String __hxcpp_get_bin_dir()
 #elif defined(APPLETVOS)
     HX_CSTRING("AppleTVOS");
 #else
-  #ifdef HXCPP_M64
+  #ifdef HXCPP_ARM64
+    HX_CSTRING("LinuxArm64");
+  #elif defined(HXCPP_ARMV7)
+    HX_CSTRING("LinuxArm");
+  #elif defined(HXCPP_M64)
     HX_CSTRING("Linux64");
   #else
     HX_CSTRING("Linux");
