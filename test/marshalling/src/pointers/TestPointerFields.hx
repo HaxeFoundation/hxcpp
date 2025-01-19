0978d5b575e1e28feedd3c78c55d6d9afbc149c9
Aidan Lee <aidan.lee@evcam.com>
2025-01-19 21:48:33 +0000

    Add pointer collection tests

--- test/marshalling/src/pointers/TestPointerFields.hx
+++ test/marshalling/src/pointers/TestPointerFields.hx
@@ -37,11 +37,51 @@ class TestPointerFields extends Test {
         }
     }
 
+    function test_copying_to_var() {
+        final h = create_holder();
+        final p = h.pPtr;
+
+        p.x = 100;
+        p.y = 200;
+
+        Assert.equals(100f64, h.pPtr.x);
+        Assert.equals(200f64, h.pPtr.y);
+    }
+
+    function test_assignment() {
+        final h = create_holder();
+        final p = create_point();
+
+        h.pPtr = p;
+
+        Assert.equals(100f64, h.pPtr.x);
+        Assert.equals(200f64, h.pPtr.y);
+    }
+
+    function test_assignment_from_promoted() {
+        final h = create_holder();
+        final p = create_point();
+        final f = () -> {
+            return p.x;
+        }
+
+        f();
+
+        h.pPtr = p;
+
+        Assert.equals(100f64, h.pPtr.x);
+        Assert.equals(200f64, h.pPtr.y);
+    }
+
     function create_holder() : Holder {
         return untyped __cpp__('::holder(::hx::maths::point(), ::hx::maths::point())');
     }
 
     function create_holder_with_null() : Holder {
         return untyped __cpp__('::holder()');
+    }
+
+    function create_point() : Point {
+        return untyped __cpp__('new ::hx::maths::point(100, 200)');
     }
 }
