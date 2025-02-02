1354a5698ab4353bed0fb509c192a5082adba57a
Aidan Lee <aidan.lee@evcam.com>
2025-02-02 14:05:57 +0000

    reduce test duplication

--- test/native/tests/marshalling/managed/TestLocalNonStandardManagedClass.hx
+++ test/native/tests/marshalling/managed/TestLocalNonStandardManagedClass.hx
@@ -17,65 +17,7 @@ extern class NonStandardNamingExtern {
     static function create(number : Int) : NonStandardNamingExtern;
 }
 
-// @:include('Managed.hpp')
-// @:cpp.ManagedType({ type : 'standard_naming', namespace : [ 'foo', 'bar' ], flags : [ StandardNaming ] })
-// extern class StandardNamingExtern {
-//     function doubleNumber(input : Int) : Int;
-
-//     static function create() : StandardNamingExtern;
-// }
-
-// @:include('Managed.hpp')
-// @:cpp.ManagedType({ namespace : [ 'hx' ] })
-// extern class WithClosure {
-//     function new() : Void;
-
-//     @:native('ReturnSeven')
-//     function returnSeven() : Int;
-// }
-
-class TestLocalNonStandardManagedClass extends Test {
-
-    function test_null_object() {
-        final o : NonStandardNamingExtern = null;
-
-        Assert.isNull(o);
-    }
-
-    function test_construction() {
-        final o = new NonStandardNamingExtern();
-
-        Assert.notNull(o);
-    }
-
-    function test_equality() {
-        final o1 = new NonStandardNamingExtern();
-        final o2 = o1;
-
-        Assert.equals(o1, o2);
-    }
-
-    function test_inequality() {
-        final o1 = new NonStandardNamingExtern();
-        final o2 = new NonStandardNamingExtern();
-
-        Assert.notEquals(o1, o2);
-    }
-
-    function test_var_access() {
-        final o = new NonStandardNamingExtern();
-
-        Assert.equals(0, o.number);
-    }
-
-    function test_var_mutation() {
-        final o = new NonStandardNamingExtern();
-
-        o.number = 7;
-
-        Assert.equals(7, o.number);
-    }
-
+class TestLocalNonStandardManagedClass extends TestLocalHarness<NonStandardNamingExtern> {
     function test_static_var_access() {
         Assert.equals(300, NonStandardNamingExtern.constNumber);
     }
@@ -88,46 +30,9 @@ class TestLocalNonStandardManagedClass extends Test {
         NonStandardNamingExtern.constNumber = 300;
     }
 
-    function test_function_call() {
-        final o = new NonStandardNamingExtern();
-
-        o.number = 7;
-
-        Assert.equals(14, o.multiply(2));
-    }
-
     function test_static_function_call() {
         final o = NonStandardNamingExtern.create(7);
 
         Assert.notNull(o);
     }
-
-    function test_to_string() {
-        final o = new NonStandardNamingExtern();
-
-        Assert.equals("My Custom Managed Type", Std.string(o));
-    }
-
-    function test_null_access() {
-        final o : NonStandardNamingExtern = null;
-
-        Assert.raises(() -> o.number = 7);
-    }
-
-    // function test_type_check() {
-    //     final o : Any = new NonStandardNamingExtern();
-
-    //     Assert.isTrue(o is NonStandardNamingExtern);
-    // }
-
-    // function test_function_closures() {
-    //     final o = new WithClosure();
-    //     final f = o.returnSeven;
-
-    //     Assert.equals(7, f());
-    // }
-
-    // function test_reflection() {
-    //     //
-    // }
 }
--- test/native/tests/marshalling/managed/TestLocalStandardManagedClass.hx
+++ test/native/tests/marshalling/managed/TestLocalStandardManagedClass.hx
@@ -17,48 +17,7 @@ extern class StandardNamingExtern {
     static function create(number : Int) : StandardNamingExtern;
 }
 
-class TestLocalStandardManagedClass extends Test {
-
-    function test_null_object() {
-        final o : StandardNamingExtern = null;
-
-        Assert.isNull(o);
-    }
-
-    function test_construction() {
-        final o = new StandardNamingExtern();
-
-        Assert.notNull(o);
-    }
-
-    function test_equality() {
-        final o1 = new StandardNamingExtern();
-        final o2 = o1;
-
-        Assert.equals(o1, o2);
-    }
-
-    function test_inequality() {
-        final o1 = new StandardNamingExtern();
-        final o2 = new StandardNamingExtern();
-
-        Assert.notEquals(o1, o2);
-    }
-
-    function test_var_access() {
-        final o = new StandardNamingExtern();
-
-        Assert.equals(0, o.number);
-    }
-
-    function test_var_mutation() {
-        final o = new StandardNamingExtern();
-
-        o.number = 7;
-
-        Assert.equals(7, o.number);
-    }
-
+class TestLocalStandardManagedClass extends TestLocalHarness<StandardNamingExtern> {
     function test_static_var_access() {
         Assert.equals(300, StandardNamingExtern.constNumber);
     }
@@ -71,46 +30,9 @@ class TestLocalStandardManagedClass extends Test {
         StandardNamingExtern.constNumber = 300;
     }
 
-    function test_function_call() {
-        final o = new StandardNamingExtern();
-
-        o.number = 7;
-
-        Assert.equals(14, o.multiply(2));
-    }
-
     function test_static_function_call() {
         final o = StandardNamingExtern.create(7);
 
         Assert.notNull(o);
     }
-
-    function test_to_string() {
-        final o = new StandardNamingExtern();
-
-        Assert.equals("My Custom Managed Type", Std.string(o));
-    }
-
-    function test_null_access() {
-        final o : StandardNamingExtern = null;
-
-        Assert.raises(() -> o.number = 7);
-    }
-
-    // function test_type_check() {
-    //     final o : Any = new NonStandardNamingExtern();
-
-    //     Assert.isTrue(o is NonStandardNamingExtern);
-    // }
-
-    // function test_function_closures() {
-    //     final o = new WithClosure();
-    //     final f = o.returnSeven;
-
-    //     Assert.equals(7, f());
-    // }
-
-    // function test_reflection() {
-    //     //
-    // }
 }
