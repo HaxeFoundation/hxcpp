82dae602be5ecdcce30ee646722186a41a9e1a58
Tobiasz Laskowski <tobil4sk@outlook.com>
2025-08-27 19:03:40 +0100

    Add checks for String::create with length 0

--- test/regression/Issue849/Main.hx
+++ test/regression/Issue849/Main.hx
@@ -7,4 +7,15 @@ function main() {
 
 	// char16_t
 	trace(untyped __cpp__('::String::create(u"Hello world")'));
+
+	// explicit 0 length
+
+	// char
+	trace(untyped __cpp__('::String::create("Hello world", 0)'));
+
+	// wchar_t
+	trace(untyped __cpp__('::String::create(L"Hello world", 0)'));
+
+	// char16_t
+	trace(untyped __cpp__('::String::create(u"Hello world", 0)'));
 }
