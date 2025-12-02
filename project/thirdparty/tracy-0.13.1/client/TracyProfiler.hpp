1f30d65c1a56a3e5d3297e744fc6f96659e15785
Hyper_ <40342021+NotHyper-474@users.noreply.github.com>
2025-12-02 16:35:36 -0300

    Update Tracy to 0.13.1 protocol

--- project/thirdparty/tracy-0.13.1/client/TracyProfiler.hpp
+++ project/thirdparty/tracy-0.13.1/client/TracyProfiler.hpp
@@ -58,6 +58,9 @@ TRACY_API bool IsProfilerStarted();
 #  define TracyIsStarted true
 #endif
 
+TRACY_API bool BeginSamplingProfiling();
+TRACY_API void EndSamplingProfiling();
+
 class GpuCtx;
 class Profiler;
 class Socket;
@@ -251,6 +254,9 @@ public:
         return 0;  // unreachable branch
 #endif
     }
+
+    bool BeginSamplingProfiling();
+    void EndSamplingProfiling();
 
     tracy_force_inline uint32_t GetNextZoneId()
     {
@@ -720,6 +726,9 @@ public:
 #ifdef TRACY_FIBERS
     static tracy_force_inline void EnterFiber( const char* fiber, int32_t groupHint )
     {
+#ifdef TRACY_ON_DEMAND
+        if( !GetProfiler().IsConnected() ) return;
+#endif
         TracyQueuePrepare( QueueType::FiberEnter );
         MemWrite( &item->fiberEnter.time, GetTime() );
         MemWrite( &item->fiberEnter.fiber, (uint64_t)fiber );
@@ -729,6 +738,9 @@ public:
 
     static tracy_force_inline void LeaveFiber()
     {
+#ifdef TRACY_ON_DEMAND
+        if( !GetProfiler().IsConnected() ) return;
+#endif
         TracyQueuePrepare( QueueType::FiberLeave );
         MemWrite( &item->fiberLeave.time, GetTime() );
         TracyQueueCommit( fiberLeave );
@@ -991,7 +1003,6 @@ private:
 
     double m_timerMul;
     uint64_t m_resolution;
-    uint64_t m_delay;
     std::atomic<int64_t> m_timeBegin;
     uint32_t m_mainThread;
     uint64_t m_epoch, m_exectime;
@@ -1032,6 +1043,7 @@ private:
     std::atomic<bool> m_isConnected;
 #ifdef TRACY_ON_DEMAND
     std::atomic<uint64_t> m_connectionId;
+    std::atomic<bool> m_symbolsBusy;
 
     TracyMutex m_deferredLock;
     FastVector<QueueItem> m_deferredQueue;
