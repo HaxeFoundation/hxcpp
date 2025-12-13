d315b3a018121257ffc94fc2fc10249d07ded540
Aidan Lee <aidan.lee@evcam.com>
2025-12-13 17:25:08 +0000

    guard including new marshal headers

--- include/hxcpp.h
+++ include/hxcpp.h
@@ -351,13 +351,15 @@ typedef PropertyAccessMode PropertyAccess;
 #endif
 #include <hx/StdLibs.h>
 #include <cpp/Pointer.h>
-#include <cpp/marshal/Boxed.hpp>
-#include <cpp/marshal/ValueType.hpp>
-#include <cpp/marshal/PointerType.hpp>
-#include <cpp/marshal/ValueReference.hpp>
-#include <cpp/marshal/PointerReference.hpp>
-#include <cpp/marshal/View.hpp>
-#include <cpp/marshal/Marshal.hpp>
+#ifdef HXCPP_API_LEVEL>=500
+    #include <cpp/marshal/Boxed.hpp>
+    #include <cpp/marshal/ValueType.hpp>
+    #include <cpp/marshal/PointerType.hpp>
+    #include <cpp/marshal/ValueReference.hpp>
+    #include <cpp/marshal/PointerReference.hpp>
+    #include <cpp/marshal/View.hpp>
+    #include <cpp/marshal/Marshal.hpp>
+#endif
 #include <hx/Native.h>
 #include <hx/Operators.h>
 #include <hx/Functions.h>
