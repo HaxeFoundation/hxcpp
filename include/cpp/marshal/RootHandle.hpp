e228d5865092789b8b3e2e389db0a62650024c0e
Aidan Lee <aidan.lee@evcam.com>
2026-01-16 21:59:31 +0000

    Null checking

--- include/cpp/marshal/RootHandle.hpp
+++ include/cpp/marshal/RootHandle.hpp
@@ -6,6 +6,11 @@ inline cpp::marshal::RootHandle::RootHandle(::hx::Object** obj) : object(obj) {}
 
 inline cpp::marshal::RootHandle cpp::marshal::RootHandle::create(::Dynamic obj)
 {
+	if (null() == obj)
+	{
+		hx::NullReference("Dynamic", false);
+	}
+
 	auto root = new hx::Object* { obj.mPtr };
 
 	hx::GCAddRoot(root);
@@ -15,6 +20,11 @@ inline cpp::marshal::RootHandle cpp::marshal::RootHandle::create(::Dynamic obj)
 
 inline cpp::marshal::RootHandle cpp::marshal::RootHandle::fromVoidPointer(cpp::Pointer<void> ptr)
 {
+	if (nullptr == ptr.ptr)
+	{
+		hx::NullReference("Dynamic", false);
+	}
+
 	return RootHandle(static_cast<hx::Object**>(ptr.ptr));
 }
 
