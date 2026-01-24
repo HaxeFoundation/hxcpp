90732bb459de2cdfccb339fc0f89a9ed0be6fcfc
Aidan Lee <aidan.lee@evcam.com>
2026-01-24 20:08:22 +0000

    Add new mutex and condition types

---
+++ src/hx/thread/ConditionVariable.cpp
@@ -0,0 +1,62 @@
+#include <hxcpp.h>
+#include "hx/thread/ConditionVariable.hpp"
+#include <mutex>
+#include <condition_variable>
+#include <chrono>
+
+struct hx::thread::ConditionVariable_obj::Impl
+{
+	std::mutex mutex;
+	std::condition_variable_any condition;
+};
+
+hx::thread::ConditionVariable_obj::ConditionVariable_obj() : impl(new Impl())
+{
+	GCSetFinalizer(this, [](hx::Object* obj)
+	{
+		auto condition = reinterpret_cast<ConditionVariable_obj*>(obj);
+
+		delete condition->impl;
+	});
+}
+
+void hx::thread::ConditionVariable_obj::acquire()
+{
+	hx::AutoGCFreeZone zone;
+
+	impl->mutex.lock();
+}
+
+bool hx::thread::ConditionVariable_obj::tryAcquire()
+{
+	return impl->mutex.try_lock();
+}
+
+void hx::thread::ConditionVariable_obj::release()
+{
+	impl->mutex.unlock();
+}
+
+void hx::thread::ConditionVariable_obj::wait()
+{
+	hx::AutoGCFreeZone zone;
+
+	impl->condition.wait(impl->mutex);
+}
+
+bool hx::thread::ConditionVariable_obj::timedWait(double seconds)
+{
+	hx::AutoGCFreeZone zone;
+
+	return impl->condition.wait_for(impl->mutex, std::chrono::duration<double>(seconds)) == std::cv_status::timeout;
+}
+
+void hx::thread::ConditionVariable_obj::signal()
+{
+	impl->condition.notify_one();
+}
+
+void hx::thread::ConditionVariable_obj::broadcast()
+{
+	impl->condition.notify_all();
+}
