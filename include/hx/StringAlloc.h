0b7af0d9bb0c663f062ca49a5263bb0aa32e9b0c
Tobiasz Laskowski <tobil4sk@outlook.com>
2026-03-14 03:39:16 +0000

    Fix overrides in cffi

--- include/hx/StringAlloc.h
+++ include/hx/StringAlloc.h
@@ -16,6 +16,14 @@ class IStringAlloc
       ~IStringAlloc(){};
 };
 
+#ifndef HXCPP_OVERRIDE
+   #if __cplusplus >= 201103L || (defined(_MSC_VER) && _MSVC_LANG >= 201103L)
+      #define HXCPP_OVERRIDE override
+   #else
+      #define HXCPP_OVERRIDE
+   #endif
+#endif
+
 template<int STACK>
 class StringAlloc : public IStringAlloc
 {
